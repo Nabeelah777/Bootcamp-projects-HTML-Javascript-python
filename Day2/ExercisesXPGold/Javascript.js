dbfa202f3309caebaa55d4bcf3e811ca2d6f229e
Nabeelah <maudarbocusnabeelah@gmail.com>
2022-11-15 17:50:26 +0400

    exercise XP Gold

---
+++ Day2/ExercisesXPGold/Javascript.js
@@ -0,0 +1,12 @@
+//Exercises XP Gold
+//Exercise 1 : Favorite Color
+
+let sentence = ["my","favorite","color","is","blue"];
+
+console.log(sentence.join(" "))
+
+
+//Exercise 2 : Mixup
+let str1 = "mix";
+let str2 = "pod";
+
